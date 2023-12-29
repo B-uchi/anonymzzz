b83ec0b0a54e2c573222d54a309bf431e959a328
Ibuchukwu Umeano <ibumeano@gmail.com>
2023-12-29 21:53:29 +0100

    fixed send message ui

--- frontend/src/pages/sendMessage.jsx
+++ frontend/src/pages/sendMessage.jsx
@@ -1,9 +1,13 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import anonymous from "../assets/anonymous.png";
+import { Toaster, toast } from "sonner";
+import { set } from "mongoose";
 
 const SendMessage = () => {
   const [loading, setLoading] = useState(true);
+  const [processing, setProcessing] = useState(false);
   const [valid, setValid] = useState(true);
   const [error, setError] = useState(false);
   const [message, setMessage] = useState("");
@@ -54,6 +58,7 @@ const SendMessage = () => {
   }, []);
 
   const sendMessage = () => {
+    setProcessing(true);
     if (message != null) {
       const request = {
         url: "https://anonymzzz-server.vercel.app/user/sendMessage",
@@ -66,16 +71,20 @@ const SendMessage = () => {
       axios
         .request(request)
         .then((response) => {
-          alert("successfully sent");
+          setProcessing(false);
+          toast.success("Message sent successfully!");
           setMessage("");
         })
         .catch((e) => {
+          setProcessing(false);
+          toast.error(e.response.data.message);
           console.log(e.response.data.message);
         });
     }
   };
   return (
-    <div className="bg-[#f1f1f1] w-[100%] h-[100vh] flex justify-center items-center">
+    <div className="bg-[#f8f7f4] w-[100%] h-[100vh] flex justify-center items-center relative">
+      <Toaster richColors position="top-right" />
       {loading ? (
         <div className="spinner">
           <div></div>
@@ -90,51 +99,61 @@ const SendMessage = () => {
           <div></div>
         </div>
       ) : error ? (
-        <div className="flex flex-col max-w-[100%] w-full h-[100vh] justify-center items-center">
-          <div className="bg-[#0d0c22] w-[100%] md:w-[500px] send-container rounded-lg p-3 text-white  shadow-black shadow-md">
-            <p className="text-center">An error occured...</p>
-          </div>
+        <div className="">
+          <h1 className="text-2xl text-center w-full p-1 font-bold mb-4">
+            An error occured
+          </h1>
         </div>
       ) : valid ? (
-        <div className="flex flex-col max-w-[100%] w-full h-[100vh] justify-center items-center">
-          <div className="bg-[#0d0c22] w-[100%] md:w-[500px] send-container rounded-lg p-3 text-white  shadow-black shadow-md">
-            <div className="border h-full p-2 rounded-md">
-              <h2 className="m-0 text-2xl">Anonymzzz</h2>
-              <small className="-mt-10">
-                Let your hidden thoughts run wild!!
-              </small>
-              <div className="mt-5 text-md">
-                <p>Send an anonymouzz message to: {username}</p>
-                <textarea
-                  value={message}
-                  onChange={(e) => {
-                    setMessage(e.target.value);
-                  }}
-                  rows="10"
-                  className="resize-none text-black p-1 rounded-md w-[100%]"
-                ></textarea>
-                <div className="flex w-full justify-center mt-3">
-                  <button
-                    on
-                    className="bg-blue-500 p-2 rounded-sm cursor-pointer px-4"
-                    onClick={() => {
-                      sendMessage();
-                    }}
-                  >
-                    Send
-                  </button>
-                </div>
+        <div className="p-2 w-full md:w-1/3 flex relative flex-col items-center">
+          {processing ? (
+            <div className="bg-[rgba(255,255,255,0.7)] w-full h-full absolute flex justify-center items-center z-50">
+              <div className="spinner">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
               </div>
             </div>
+          ) : (
+            ""
+          )}
+          <div className="">
+            <img src={anonymous} alt="Anonymzzz logo" className="w-[90px]" />
           </div>
+          <h1 className="text-2xl text-center w-full p-1 font-bold mb-4">
+            Send a message to: {username}
+          </h1>
+          <textarea
+            className="w-full resize-none h-[20vh] p-2 border-2 border-gray-300 rounded-lg outline-none"
+            placeholder="Enter your message"
+            required
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          ></textarea>
+          <button
+            className="bg-[#484672] text-white border-[1px] border-gray-300 rounded-md p-2 mt-4"
+            onClick={sendMessage}
+          >
+            Send
+          </button>
         </div>
       ) : (
-        <div className="flex flex-col max-w-[100%] w-full h-[100vh] justify-center items-center">
-          <div className="bg-[#0d0c22] w-[100%] md:w-[500px] send-container rounded-lg p-3 text-white  shadow-black shadow-md">
-            <p className="text-center">The link you provided is invalid</p>
-          </div>
+        <div className="">
+          <h1 className="text-2xl text-center w-full p-1 font-bold mb-4">
+            This link is invalid
+          </h1>
         </div>
       )}
+      <small className="bottom-0 absolute right-0 p-2">
+        Made with &#10084; by Buchi
+      </small>
     </div>
   );
 };
